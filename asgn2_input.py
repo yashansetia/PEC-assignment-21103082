c4e252f3218b8c5b02e00b35ace49323b3b01ee1
yashansetia <96912414+yashansetia@users.noreply.github.com>
2022-01-21 10:22:39 +0530

    Add files via upload

---
+++ asgn2_input.py
@@ -0,0 +1,85 @@
+#Question1
+
+string1 = "Python is a case sensitive language"
+
+print("(a)Length of string is",len(string1))  #Find Length of string
+
+print("(b)The reverse string is",string1[-1::-1]) #Reverse the order of string
+
+string2=string1[10:26] # Store "a case sensitive" in another string
+
+string2.replace("a case sensitive","object oriented") #Replace "a case sensitive" With "object oriented"
+
+print("(e)The index of substring a is ",string1.find('a')) #Find index of substring “a”
+
+print("(f)The Original String After Removing Whitespaces is",string1.replace(" ","")
+
+
+#Question2
+
+#Get Information From User
+Name = str(input("Enter Your Name "))
+SID  = int(input("Enter Your SID "))
+Department=str(input("Enter Your Department "))
+CGPA = float(input("Enter Your CGPA "))
+
+print("Hey,%s"%Name,"Here!")
+print("My SID is %d" %SID)
+print("I am from %s"%Department,"and my CGPA is %f"%CGPA)
+
+
+#Question3
+
+a=56
+b=10
+
+print("(a)Value of a&b=",a&b)
+print("(b)value of a|b=",a|b)
+print("(c)value of a^b=",a^b)
+print("(d)value of a after left shifting 2 bits is",a<<2)
+print("   value of b after left shifting 2 bits is",b<<2)
+print("(e)value of a after right shifting 2 bits is",a>>2)
+print("   value of b after right shifting 4 bits is",b>>2)
+
+
+#Queation4:Print the Greatest of Three Numbers
+
+#get three numbers from users
+a=int(input("num1="))
+b=int(input("num2="))
+c=int(input("num3="))
+
+if a>=b>=c or a>=c>=b:
+  Greatest=a
+elif b>=a>=c or b>=c>=a:
+  Greatest=b
+elif c>=a>=b or c>=b>=a:
+  Greatest=c
+  
+#Print Greatest of Three Numbers
+print("Greatest of three numbers is",Greatest)
+
+#Question5
+
+#Take input from user
+string=input("Enter a string ")
+if (string.find('name') !=-1):
+    print ("Yes")
+else:
+    print ("No")
+
+
+#Question6
+
+#Take length of sides from user
+a=int(input("Enter First side of Triangle "))
+b=int(input("Enter Second side of Triangle "))
+c=int(input("Enter Third side of Triangle "))
+if(a>(b+c)):
+    print("No")
+elif(b>(a+c)):
+    print("No")
+elif(c>(a+b)):
+    print("No")
+else:
+    print("Yes")
---
+++ asgn2_output.py
@@ -0,0 +1,52 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques1.py =
+(a)Length of string is 35
+(b)The reverse string is egaugnal evitisnes esac a si nohtyP
+(e)The index of substring a is  10
+(f)The Original String After Removing Whitespaces is Pythonisacasesensitivelanguage
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques2.py =
+Enter Your Name Yashan Setia
+Enter Your SID 21103082
+Enter Your Department CSE
+Enter Your CGPA 9.6
+Hey,Yashan Setia Here!
+My SID is 21103082
+I am from CSE and my CGPA is 9.600000
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques3.py =
+(a)Value of a&b= 8
+(b)value of a|b= 58
+(c)value of a^b= 50
+(d)value of a after left shifting 2 bits is 224
+   value of b after left shifting 2 bits is 40
+(e)value of a after right shifting 2 bits is 14
+   value of b after right shifting 4 bits is 2
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques4.py =
+num1=56
+num2=78
+num3=24
+Greatest of three numbers is 78
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques5.py =
+Enter a string name
+Yes
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques5.py =
+Enter a string yashan
+No
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques6.py =
+Enter First side of Triangle 6
+Enter Second side of Triangle 8
+Enter Third side of Triangle 10
+Yes
+
+= RESTART: C:\Users\91830\Desktop\PEC\computer assignment\assignment2\ques6.py =
+Enter First side of Triangle 2
+Enter Second side of Triangle 3
+Enter Third side of Triangle 7
+No
